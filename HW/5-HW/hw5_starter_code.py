c3550be6872238a8c9b09d6f2f338ebfb65e1086
Joel Carlson <cjoeld2024@gmail.com>
2024-02-22 09:38:19 -0700

    updated gitignore

--- HW/5-HW/hw5_starter_code.py
+++ HW/5-HW/hw5_starter_code.py
@@ -1,14 +1,13 @@
 import tempfile
 import os
 from silence_tensorflow import silence_tensorflow
-silence_tensorflow()
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import os
 import zipfile
 import tensorflow_model_optimization as tfmot
-import tensorflow.keras
+from tensorflow import keras
 
 ################## STARTER CODE GIVE TO STUDENTS
 def convert_model_tflite(model):
@@ -99,7 +98,7 @@ model.fit(
   validation_split=0.1,
 )
 
-_, baseline_model_file = model.evaluate(test_images, test_labels, verbose=0)
+_, baseline_model_accuracy = model.evaluate(test_images, test_labels, verbose=0)
 
 model.save('mnist_model.h5', include_optimizer=True)
 
@@ -137,3 +136,79 @@ pruning_params = {
 
 # finish setting up how fine tuning is working
 # compile and fit the pruning model
+
+# input params aer going ot be my starting model and the pruning parameters
+
+model_pruned = prune_low_magnitude(model, **pruning_params)
+
+# compile the new prunable model
+model_pruned.compile(optimizer='adam', 
+                     loss=keras.losses.SparseCategoricalCrossentropy(from_logits = True), 
+                     metrics=['accuracy'])
+
+
+callbacks = [
+    tfmot.sparsity.keras.UpdatePruningStep()
+]
+
+_,prune_model_start_accuracy = model_pruned.evaluate(test_images, test_labels, verbose=0)
+model_pruned.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, validation_split=val_size, callbacks=callbacks)
+
+_, pruned_model_accuracy_end = model_pruned.evaluate(test_images, test_labels, verbose=0)
+
+print("baseline accuracry: ", baseline_model_accuracy)
+print("prusned start acc: ", prune_model_start_accuracy)
+print("pruned end acc: ", pruned_model_accuracy_end)
+
+# save model and compare size
+# stripping off all the supporting infastructure that did the pruning
+
+model_pruned = tfmot.sparsity.keras.strip_pruning(model_pruned)
+model_pruned.save('pruned_mnist_model.h5', include_optimizer=True)
+
+print("orig model size: ",  get_gzipped_model_size('mnist_model.h5'))
+print("pruned model size: ", get_gzipped_model_size('pruned_mnist_model.h5'))
+
+
+
+# Learnining qunatization with TFLITE
+
+quantize_model = tfmot.quantization.keras.quantize_model
+
+# set up quantized model
+model_q = quantize_model(model)
+
+model_q.compile(optimizer='adam', 
+                     loss=keras.losses.SparseCategoricalCrossentropy(from_logits = True), 
+                     metrics=['accuracy'])
+
+model_q.summary()
+
+# fit my quantized model 
+model_q.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, validation_split=val_size)
+
+_, quantized_model_accuracy = model_q.evaluate(test_images, test_labels, verbose=0)
+
+
+print("baseline accuracry: ", baseline_model_accuracy)
+print("pruned end acc: ", pruned_model_accuracy_end)
+print("quantized acc: ", quantized_model_accuracy)
+
+
+
+# compare size of every H5 file
+
+print("orig model size: ",  get_gzipped_model_size('mnist_model.h5'))
+print("pruned model size: ", get_gzipped_model_size('pruned_mnist_model.h5'))
+print("quantized model size: ", get_gzipped_model_size('quantized_mnist_model.h5'))
+
+
+
+b_tflite = convert_model_tflite(model)
+p_tflite = convert_model_tflite(model_pruned)
+q_tflite = convert_model_tflite(model_q)
+
+
+print("TFLITE Baseline model: ", get_gzipped_model_size(b_tflite))
+print("TFLITE Pruned model: ", get_gzipped_model_size(p_tflite))
+print("TFLITE Quantized model: ", get_gzipped_model_size(q_tflite))
